1dc69dc0c634ee583cfa24d491675fffc209ca79
Matheus K. Westphalen <mkwestphalen@gmail.com>
2022-10-06 14:45:48 -0400

    Add more logic to false-position method

--- roots.for
+++ roots.for
@@ -17,9 +17,17 @@
       PROGRAM roots
 
 * Variable Declaration/Initialization
-        INTEGER :: polyOrder
-        INTEGER :: aTerm, bTerm, cTerm, dTerm = 0
-        INTEGER :: tolerance
+        INTEGER :: numOfTerms
+        REAL :: aTerm, bTerm, cTerm, dTerm = 0.0
+        INTEGER :: aPower, bPower, cPower, dPower = 0.0
+        INTEGER :: tolerance, absError
+        REAL :: a = -5
+        REAL :: b = 5
+        REAL :: c, cBefore = 0
+        REAL :: f_of_a, f_of_b, f_of_c
+        REAL :: polynomial
+        DIMENSION polynomial(8)
+        DATA polynomial/0,0,0,0,0,0,0,0/
 
 * Prompt
         WRITE(*,*) 'With the help of the false-position method, this'
@@ -31,9 +39,9 @@
 
 * Getting polynomial order
 1     WRITE(*,11) 
-11    FORMAT('Order of Polynomial (less than 4): ', $)
-      READ(*,*, iostat=ierror) polyOrder
-      IF (ierror .NE. 0 .OR. polyOrder > 4 .OR. polyOrder < 0) THEN
+11    FORMAT('Number of terms (less than 4): ', $)
+      READ(*,*, iostat=ierror) numOfTerms
+      IF (ierror .NE. 0 .OR. numOfTerms > 4 .OR. numOfTerms < 0) THEN
         WRITE(*,*) 'Error: Wrong input type. Try again.'
         WRITE(*,*)
         GO TO 1
@@ -41,39 +49,90 @@
       WRITE(*,*)
 
 * Getting polynomial terms
-      CALL setTerm(aTerm)
-      CALL setTerm(bTerm)
-      CALL setTerm(cTerm)
-      IF (polyOrder .EQ. 4) THEN
-        CALL setTerm(dTerm)
-      ENDIF
+      CALL setTerm(polynomial(1), polynomial(2))
+      IF (numOfTerms == 2) THEN
+            CALL setTerm(polynomial(3), polynomial(4))
+      ELSEIF (numOfTerms == 3) THEN
+            CALL setTerm(polynomial(3), polynomial(4))
+            CALL setTerm(polynomial(5), polynomial(6))
+      ELSEIF (numOfTerms == 4) THEN
+            CALL setTerm(polynomial(3), polynomial(4))
+            CALL setTerm(polynomial(5), polynomial(6))
+            CALL setTerm(polynomial(7), polynomial(8))
+      ENDIF 
       
 * Getting the tolerance to stop iterating
-3     WRITE(*,13)
-13    FORMAT('Enter the tolerance: ', $)
+4     WRITE(*,14)
+14    FORMAT('Enter the tolerance: ', $)
       READ(*,*, iostat=ierror) tolerance
       IF (ierror .NE. 0) THEN
             WRITE(*,*) 'Error: Wrong input type. Try again.'
-            write(*,*)
-            GO TO 3
+            WRITE(*,*)
+            GO TO 4
       ENDIF
 
-      
+* Displaying equation to the user before finding the roots
+      WRITE(*,*) 
+*     TODO
+
+* Staring iterations to find root
+      DO WHILE(absError > tolerance)
+* Solving for c from secant line equation between f(a) and f(b)
+            CALL solve(f_of_a,a,polynomial)
+            CALL solve(f_of_b,b,polynomial)
+* If f(a) * f(b) >= 0, then it means there's zero or multiple roots
+* present, and the false-position method won't be helpful...
+            IF (f_of_a * f_of_b >= 0) THEN
+                  WRITE(*,*) 'The polynomial has 0 or multiple roots'
+                  WRITE(*,*) '
+            c = ((a*f_of_b)-(b*f_of_a)) / (f_of_b - f_of_a)
+
+* Defining whether the root is to the left or to the right of c
+            CALL solve(f_of_c,c,polynomial)
+            IF (f_of_c == 0) THEN
+*                 FOUND THE ROOT
+            ELSE IF (f_of_a * f_of_c < 0) THEN
+                  b = c
+            ELSE IF (f_of_b * f_of_c < 0) THEN
+                  a = c
+            ENDIF
+      ENDDO
 
       STOP
       END
 
 ****************
 * This soubroutine gets the user input for each term in a polynomial
-      SUBROUTINE setTerm(term)
-        INTEGER :: term;
+      SUBROUTINE setTerm(term, power)
+        REAL :: term, power
 2       WRITE(*,12)
-12      FORMAT('Enter term : ', $)
+12      FORMAT('Enter term: ', $)
         READ(*,*, iostat=ierror) term
         IF (ierror .NE. 0) THEN
             WRITE(*,*) 'Error: Wrong input type. Try again.'
             WRITE(*,*)
             GO TO 2
         ENDIF
+3       WRITE(*,13)
+13      FORMAT('Enter power: ', $)
+        READ(*,*,iostat=ierror) power
+        IF (ierror .NE. 0 .OR. power > 4 .OR. power < 1) THEN
+            WRITE(*,*) 'Error: Wrong input type. Try again.'
+            WRITE(*,*)
+            GO TO 3
+        ENDIF
       WRITE(*,*)
       END
+
+**************
+* This subroutine solves for f(x) given x and the terms and
+* powers of the polynomial
+      SUBROUTINE solve(f_of_x,x,polynomial)
+      REAL f_of_x, x
+      REAL :: polynomial
+      DIMENSION polynomial(8)
+
+      f_of_x = polynomial(1)*(x**polynomial(2)) + polynomial(3)*
+    &  (x**polynomial(4)) + polynomial(5)*(x**polynomial(6)) +
+    & polynomial(7)*(x**polynomial(8))
+     END
